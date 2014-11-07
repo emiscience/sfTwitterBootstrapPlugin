db66df1e8d28568be39ad0377bf3148a7029b678
Christophe Moine <cmoine@tf1.fr>
2014-11-07 14:39:52 +0100

    Migrate navbar

--- modules/sfTwitterBootstrap/templates/_header.php
+++ modules/sfTwitterBootstrap/templates/_header.php
@@ -7,27 +7,43 @@ use_helper('I18N');
 /** @var string|null Link to the action (for breadcrumbs) */ $action_link = $sf_data->getRaw('action_link');
 
 ?>
-<div class="navbar navbar-fixed-top <?php echo sfConfig::get('sf_environment'); ?>">
-  <div class="navbar-inner">
+<nav class="navbar navbar-default navbar-fixed-top <?php echo sfConfig::get('sf_environment'); ?>" role="navigation">
     <div class="container-fluid">
-      <a class="brand" href="<?php echo url_for('@homepage') ?>"><?php echo sfTwitterBootstrap::getProperty('site'); ?></a>
 
-<?php if ($sf_user->isAuthenticated()): ?>
-        <?php include_partial('sfTwitterBootstrap/menu', array('items' => $items, 'categories' => $categories)); ?>
-        <p class="logout pull-right">
-          <?php echo link_to('<i class="icon-off icon-white"></i>', sfTwitterBootstrap::getProperty('logout_route'), array('title' => __('Logout'))) ?>
-        </p>
-        <p class="logged pull-right">
-          <i class="icon-user icon-white"></i>&nbsp;
-          <?php echo __('Logged in as') ?> <a href="#"><?php echo $sf_user->__toString(); ?></a>
-        </p>
-<?php endif; // if user is authenticated ?>
-      </div>
+        <div class="navbar-header">
+
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?php echo url_for('@homepage') ?>">
+                <?php echo sfTwitterBootstrap::getProperty('site'); ?>
+            </a>
+
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <?php if ($sf_user->isAuthenticated()): ?>
+                <?php include_partial('sfTwitterBootstrap/menu', array('items' => $items, 'categories' => $categories)); ?>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <?php echo link_to('<i class="icon-off icon-white"></i>', sfTwitterBootstrap::getProperty('logout_route'), array('title' => __('Logout'))) ?>
+                    </li>
+
+                    <li>
+                        <i class="icon-user icon-white"></i>&nbsp;
+                        <?php echo __('Logged in as') ?> <a href="#"><?php echo $sf_user->__toString(); ?></a>
+                    </li>
+                </ul>
+            <?php endif; // if user is authenticated ?>
+        </div>
     </div>
-</div>
+</nav>
 
 <?php if ($sf_user->isAuthenticated() && sfTwitterBootstrap::getProperty('include_path')): ?>
-<ul class='breadcrumb mbs'>
+<ol class="breadcrumb">
   <li>
     <?php echo link_to(__(sfTwitterBootstrap::getProperty('breadcrumb_root_name')), sfTwitterBootstrap::getProperty('dashboard_url')) ?>
   </li>
@@ -38,7 +54,7 @@ use_helper('I18N');
       <?php echo link_to(ucfirst(__($action_link_name)), $action_link); ?>
     <?php endif ?></li>
   <?php endif; ?>
-</ul>
+</ol>
 <?php endif; // if breadcrumbs are enabled ?>
 
 <?php include_component_slot('sf_twitter_bootstrap_permanent_slot');
--- modules/sfTwitterBootstrap/templates/_menu.php
+++ modules/sfTwitterBootstrap/templates/_menu.php
@@ -5,56 +5,56 @@ use_helper('I18N');
 ?>
 
 <?php if (count($items)): ?>
-  <ul class="nav">
-    <?php if (sfTwitterBootstrap::hasItemsMenu($items)): ?>
-      <li class="dropdown" data-dropdown="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-          Menu
-          <b class="caret"></b>
-        </a>
-        <ul class="dropdown-menu">
-          <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $items, 'items_in_menu' => true)); ?>
-        </ul>
-      </li>
-    <?php endif; ?>
-    <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $items, 'items_in_menu' => false)); ?>
-  </ul>
+    <ul class="nav">
+        <?php if (sfTwitterBootstrap::hasItemsMenu($items)): ?>
+            <li class="dropdown" data-dropdown="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    Menu
+                    <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                   <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $items, 'items_in_menu' => true)); ?>
+                </ul>
+            </li>
+        <?php endif; ?>
+        <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $items, 'items_in_menu' => false)); ?>
+    </ul>
 <?php endif; ?>
 
 <?php if (count($categories)): ?>
-  <ul class="nav">
+    <ul class="nav navbar-nav">
 
-     <?php foreach ($categories as $name => $category): ?>
-         <?php if (sfTwitterBootstrap::hasPermission($category, $sf_user)): ?>
+        <?php foreach ($categories as $name => $category): ?>
+            <?php if (sfTwitterBootstrap::hasPermission($category, $sf_user)): ?>
 
-            <?php if (isset($category['url'])) : ?>
-            <li>
-                <a href="<?php echo url_for($category['url']); ?>">
-                <?php echo __(isset($category['name']) ? $category['name'] : $name) ?>
-                </a>
-            <?php else : ?>
-            <li class="dropdown">
-                <a data-toggle="dropdown" href="#" class="dropdown-toggle" >
-                    <?php echo __(isset($category['name']) ? $category['name'] : $name) ?>
-                    <b class="caret"></b>
-                </a>
-                <?php if (isset($category['items']) && sfTwitterBootstrap::hasItemsMenu($category['items'])): ?>
-                <ul class="dropdown-menu" role="menu">
-                    <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $category['items'], 'items_in_menu' => true)) ?>
-                </ul>
+                <?php if (isset($category['url'])) : ?>
+                    <li>
+                        <a href="<?php echo url_for($category['url']); ?>">
+                        <?php echo __(isset($category['name']) ? $category['name'] : $name) ?>
+                        </a>
+                    </li>
+                <?php else : ?>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <?php echo __(isset($category['name']) ? $category['name'] : $name) ?>
+                            <span class="caret"></span>
+                        </a>
+                        <?php if (isset($category['items']) && sfTwitterBootstrap::hasItemsMenu($category['items'])): ?>
+                        <ul class="dropdown-menu" role="menu">
+                            <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $category['items'], 'items_in_menu' => true)) ?>
+                        </ul>
+                        <?php endif; ?>
+                    </li>
                 <?php endif; ?>
             <?php endif; ?>
-            </li>
-         <?php endif; ?>
-     <?php endforeach; ?>
+        <?php endforeach; ?>
 
-    <?php foreach ($categories as $name => $category): ?>
-      <?php if (isset($category['items'])): ?>
-        <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $category['items'], 'items_in_menu' => false)) ?>
-      <?php endif; ?>
-    <?php endforeach; ?>
-
-  </ul>
+        <?php foreach ($categories as $name => $category): ?>
+            <?php if (isset($category['items'])): ?>
+                <?php include_partial('sfTwitterBootstrap/menu_list', array('items' => $category['items'], 'items_in_menu' => false)) ?>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ul>
 <?php elseif (!count($items)): ?>
-  <?php echo __('sfTwitterBootstrap is not configured.'); ?>
+    <?php echo __('sfTwitterBootstrap is not configured.'); ?>
 <?php endif;
