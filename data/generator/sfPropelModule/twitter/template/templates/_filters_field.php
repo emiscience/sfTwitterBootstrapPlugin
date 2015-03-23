025a399d0bc4cc3cae47db7ecad6c8621e9da124
Gabriel Fernandes <gfernandes.davidson@tf1.fr>
2015-03-23 15:02:20 +0100

    Use form-control by default in filters_field

--- data/generator/sfPropelModule/twitter/template/templates/_filters_field.php
+++ data/generator/sfPropelModule/twitter/template/templates/_filters_field.php
@@ -3,8 +3,19 @@
 [?php elseif ($field->isComponent()): ?]
   [?php include_component('<?php echo $this->getModuleName() ?>', $name, array('type' => 'filter', 'form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?]
 [?php else: ?]
+    [?php $attributes = $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes ?]
+    [?php if ($field->getType() == "Boolean"): ?]
+        [?php $attributes = $attributes ?]
+    [?php elseif (isset($attributes['class'])): ?]
+        [?php $attributes['class'] .= " form-control" ?]
+    [?php else: ?]
+        [?php $attributes['class'] = " form-control" ?]
+    [?php endif; ?]
+
+    [?php $attributes['style'] = "width:95%" ?]
+
   [?php echo $form[$name]->renderError() ?]
-  [?php echo $form[$name]->render($attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes) ?]
+  [?php echo $form[$name]->render($attributes) ?]
   [?php if ($help || $help = $form[$name]->renderHelp()): ?]
     <span class="help-block">[?php echo __($help, array(), '<?php echo $this->getI18nCatalogue() ?>') ?]</span>
   [?php endif; ?]
