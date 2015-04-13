cb56a0b5962a1d8e44deaa33ca07bbc32ebacf61
Christophe Moine <cmoine@tf1.fr>
2015-04-13 14:38:08 +0200

    Ensure the schema formatter is always used

--- data/generator/sfPropelModule/twitter/template/templates/_form_field.php
+++ data/generator/sfPropelModule/twitter/template/templates/_form_field.php
@@ -3,29 +3,7 @@
 [?php elseif ($field->isComponent()): ?]
   [?php include_component('<?php echo $this->getModuleName() ?>', $name, array('form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?]
 [?php else: ?]
-  <div class="control-group [?php echo $class ?][?php $form[$name]->hasError() and print ' error' ?]">
 
-      <div class="col-sm-2 control-label">
-        [?php echo $form[$name]->renderLabel($label) ?]
-      </div>
+    [?php echo $form[$name]->renderRow() ?]
 
-    <div class="col-sm-8">
-        [?php $attributes = $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes ?]
-        [?php if ($field->getType() == "Boolean"): ?]
-            [?php $attributes = $attributes ?]
-        [?php elseif (isset($attributes['class'])): ?]
-            [?php $attributes['class'] .= " form-control" ?]
-        [?php else: ?]
-            [?php $attributes['class'] = " form-control" ?]
-        [?php endif; ?]
-        [?php echo $form[$name]->renderError() ?]
-        [?php echo $form[$name]->render($attributes) ?]
-
-        [?php if ($help): ?]
-            <p class="help-block">[?php echo __(html_entity_decode($help), array(), '<?php echo $this->getI18nCatalogue() ?>') ?]</p>
-        [?php elseif ($help = $form[$name]->renderHelp()): ?]
-            <p class="help-block">[?php echo $help ?]</p>
-        [?php endif; ?]
-    </div>
-  </div>
 [?php endif; ?]
